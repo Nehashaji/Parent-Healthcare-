6bf247c1865f398bd9c355c38d969b8cd400cfeb
Nehashaji <144330515+Nehashaji@users.noreply.github.com>
2025-08-11 19:06:19 +0400

    Parents Healthcare

--- home.js
+++ home.js
@@ -14,3 +14,23 @@ document.querySelectorAll('.nav-links a').forEach(link => {
     navLinks.classList.remove('active');
   });
 });
+
+document.getElementById("btn-en").addEventListener("click", function () {
+  changeLanguage('en');
+  this.classList.add("active");
+  document.getElementById("btn-ar").classList.remove("active");
+});
+
+document.getElementById("btn-ar").addEventListener("click", function () {
+  changeLanguage('ar');
+  this.classList.add("active");
+  document.getElementById("btn-en").classList.remove("active");
+});
+
+function changeLanguage(lang) {
+  var select = document.querySelector(".goog-te-combo");
+  if (select) {
+    select.value = lang;
+    select.dispatchEvent(new Event("change"));
+  }
+}
